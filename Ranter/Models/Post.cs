1684610b1fcd4646a294a23d31a452065796a047
Robert Veres <thedeveloper733@gmail.com>
2017-09-09 10:17:53 -0400

    Adding Post and User Models

---
+++ Ranter/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ranter.Models
+{
+    public class Post
+    {
+
+        [Key]
+        public int PostID { get; set; }
+        public string PostName { get; set; }
+        public DateTime PostTime { get; set; }
+
+    }
+}
---
+++ Ranter/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ranter.Models
+{
+    public class User : IdentityUser
+    {
+
+        public virtual List<Post> Posts { get; set; }
+
+    }
+}
